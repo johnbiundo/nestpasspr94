0fac354f9c85b5c1662f0830d298a5e228b7af00
John Biundo <johnfbiundo@gmail.com>
2019-08-09 10:12:42 -0700

    remove ref to auth.module, remove @nestjs/passport from package.json

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { SessionSerializer } from './session.serializer';
 import { GoogleStrategy } from './google.strategy';
 
 @Module({
   imports: [
-    UsersModule,
     PassportModule.register({
       authType: 'reauthenticate',
       prompt: 'select_account',
